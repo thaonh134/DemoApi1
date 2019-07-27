875e990ea77a3831ccbda11358fbd967676e0caa
hoang nguyen thao <mrhoang13401@gmail.com>
2019-07-27 21:11:50 +0700

    test add

--- DemoApi2/DemoApi/Controllers/V1/MediasController.cs
+++ DemoApi2/DemoApi/Controllers/V1/MediasController.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Web;
 using System.Web.Http;
+using DemoApi.Models.Medias;
 
 namespace DemoApi.Controllers.V1
 {
@@ -15,16 +16,31 @@ namespace DemoApi.Controllers.V1
     public class MediasController : DemoApiBaseController
     {
         private IUserService _userService;
+        private IMediaService _mediaService;
+        
         private IAspNetUserRepository _aspNetUserRepository;
 
-        public MediasController(IUserService userService, IAspNetUserRepository aspNetUserRepository)
+        public MediasController(IUserService userService, IMediaService mediaService, IAspNetUserRepository aspNetUserRepository)
         {
             _userService = userService;
+            _mediaService = mediaService;
             _aspNetUserRepository = aspNetUserRepository;
         }
         [Route("getAllMedias")]
         [HttpGet]
         public IHttpActionResult GetAllMedias(HttpRequestMessage requestMessage)
+        {
+            var users = _userService.GetUserInfor();
+            if (users == null)
+                return new TCErrorHttpActionResult(requestMessage, "AUTH_0001", "Phiên truy cập không được phép()");
+
+            var dataResponse = users;
+
+            return new TCSuccessHttpActionResult(requestMessage, dataResponse);
+        }
+        [Route("add")]
+        [HttpPost]
+        public IHttpActionResult Add(HttpRequestMessage requestMessage, AddMediaModel model)
         {
             var users = _userService.GetUserInfor();
             if (users == null)
